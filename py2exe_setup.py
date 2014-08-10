cb917fa229c10867c5d6507a1fc89c7e36b045dc
cd <shallweqin@163.com>
2014-08-10 20:57:13 +0800

    while generating executable file by py2exe, set 'compressed' and 'optimize' flag.

--- py2exe_setup.py
+++ py2exe_setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 import py2exe
 
-setup(options = {"py2exe":{"bundle_files":1}},
+setup(options = {"py2exe":{"bundle_files":1, 'compressed':2, 'optimize':2}},
       zipfile=None,
       windows=[{'script':"figurefreer.py",
                 'icon_resources':[(1, 'figurefreer.ico')]}])
